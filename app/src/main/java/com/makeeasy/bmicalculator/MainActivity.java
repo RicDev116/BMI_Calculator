7ff9740e73c44680f825466cbe388223cccea0dc
RicDev116 <ricardo_29leyva@outlook.com>
2022-05-19 00:39:54 -0500

    Initial commit

---
+++ app/src/main/java/com/makeeasy/bmicalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.makeeasy.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
