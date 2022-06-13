262e06ebde3d75c1ae5f3d3f8d544e8435b384a8
Ricardo Lopez Leyva <ricardo.lopez@Seguritech.local>
2022-06-13 16:24:07 -0500

    cambios

--- app/src/main/java/com/makeeasy/bmicalculator/MainActivity.java
+++ app/src/main/java/com/makeeasy/bmicalculator/MainActivity.java
@@ -10,12 +10,13 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
     private Button calculateButton;
     private RadioButton radioButtonMale;
-    private RadioButton radioButtonMale1;
     private RadioButton radioButtonFemale;
     private EditText editTextAge;
     private EditText editTextFeet;
@@ -29,15 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
         findViews();
         setButtonOnClickListener();
-        //String alertText = "This is my variable text";
-        //android.widget.Toast.makeText(this,alertText,Toast.LENGTH_SHORT).show();
 
     }
 
     private void findViews(){
         textViewResult = findViewById(R.id.text_view_result);
         calculateButton = findViewById(R.id.button_calculate);
-        radioButtonMale1 = findViewById(R.id.radio_button_male);
+        radioButtonMale = findViewById(R.id.radio_button_male);
         radioButtonFemale = findViewById(R.id.radio_button_female);
         editTextAge = findViewById(R.id.edit_text_age);
         editTextFeet = findViewById(R.id.edit_text_feet);
@@ -49,18 +48,69 @@ public class MainActivity extends AppCompatActivity {
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculateBMI();
+
+                double bmiResult = calculateBMI();
+
+                String ageText = editTextAge.getText().toString();
+
+                int age = Integer.parseInt(ageText);
+
+                if(age>=18){
+                    diaplayResult(bmiResult);
+                }else{
+                    displayGuidence(bmiResult);
+                }
             }
         });
     }
 
-    private void calculateBMI() {
-        String ageText = editTextAge.getText().toString();
+    private void displayGuidence(double bmi) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String bmiTextViewResult = decimalFormat.format(bmi);
+
+        String message;
+        if(radioButtonMale.isChecked()){
+            message = "As you are nder 18, please contult with your doctor for the healthy range for boys";
+        }else if(radioButtonFemale.isChecked()){
+            message = "As you are nder 18, please contult with your doctor for the healthy range for girls";
+        }else{
+            message = "As you are nder 18, please contult with your doctor for the healthy range";
+        }
+
+        textViewResult.setText(bmiTextViewResult + " " + message);
+    }
+
+    private double calculateBMI() {
         String feetText = editTextFeet.getText().toString();
         String inchesText= editTextInches.getText().toString();
         String weightText = editTextWeight.getText().toString();
 
-        textViewResult.setText("Age: " + ageText + ",Feet: "+ feetText + ",Inches: " + inchesText + ", Weight: " + weightText);
+        int feet = Integer.parseInt(feetText);
+        int inches = Integer.parseInt(inchesText);
+        int weight = Integer.parseInt(weightText);
+
+        int totalInches = (feet*12) + inches;
+
+        double heightInMeters = totalInches * 0.0254;
+
+        return weight / (heightInMeters * heightInMeters);
+    }
+
+    private void diaplayResult(double bmi){
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String bmiTextViewResult = decimalFormat.format(bmi);
+
+        String message;
+
+        if(bmi<18.5){
+            message = "You are a underweight";
+        }else if(bmi > 25){
+            message = "You are a overweight";
+        }else{
+            message = "You are a healthy weight";
+        }
+
+        textViewResult.setText(bmiTextViewResult + " " + message);
     }
 
 }
